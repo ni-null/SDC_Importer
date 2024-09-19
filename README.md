c727dc548d27d555607d430c74b1d0c92141f953
Ninull <admin@ninull.com>
2024-09-19 11:12:13 +0800

    check network timout

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 ### 介紹
 
-一個基於WP All Import創作的上傳更新工具
+一個基於 WP All Import 創作的上傳更新工具
 
-1. 合併survey_item_list開頭CSV檔案
+1. 合併 survey_item_list 開頭 CSV 檔案
 2. 上傳合併後的檔案
-3. 觸發API更新
+3. 觸發 API 更新
 
 ### 打包
 
@@ -15,6 +15,7 @@ pip install -r requirements.txt
 ```
 
 建立 config.json 於根目錄
+
 ```
 {
   "product_update_text": {
@@ -36,20 +37,19 @@ pip install -r requirements.txt
 ```
 
 ```
-python build.py 
+python build.py
 ```
 
 ### 運行
-初次運行會自動於同目錄下建立sdc_data檔案  
+
+初次運行會自動於同目錄下建立 sdc_data 檔案  
 ![Snipaste_2024-08-28_12-17-25](https://github.com/user-attachments/assets/a69b3762-2ba7-4510-9f8a-df1a92b2665a)
 
-
-運行log於app.log  
+運行 log 於 app.log  
 ![Snipaste_2024-08-28_12-17-31](https://github.com/user-attachments/assets/8b873c45-3b86-47df-971c-fe3ddab42728)
 
-
-
 ### 狀態
+
 需出現 Response data : {"status":200,"message":"Import # complete"} 才表示完整執行成功  
 運行中需保持網路通順  
 ![Snipaste_2024-08-28_12-16-01](https://github.com/user-attachments/assets/8cbd7a97-e54b-4675-8cac-852c61af94f6)
--- inc/cron.py
+++ inc/cron.py
@@ -11,7 +11,7 @@ def trigger_job(cron_trigger_url,cron_trigger_cancel_url,pt):
 
     for attempt in range(1, max_attempts + 1):
         try:
-            trigger_response = requests.get(cron_trigger_url) 
+            trigger_response = requests.get(cron_trigger_url, timeout=10) 
             
             if trigger_response.json().get("status") == 200 :
                 
@@ -32,6 +32,10 @@ def trigger_job(cron_trigger_url,cron_trigger_cancel_url,pt):
                 pt(f"Attempt {attempt}: Retry after {
                       retry_interval} seconds")
                 time.sleep(retry_interval)
+                
+        except (requests.ConnectionError, requests.Timeout) as e:
+            pt(f"嘗試 {attempt}/3: 網路異常 - {e}")
+            
         except requests.exceptions.RequestException as e:
             pt(f"嘗試 {attempt}/3: 請求失敗 - {e} \n")
             time.sleep(retry_interval)
@@ -40,32 +44,42 @@ def trigger_job(cron_trigger_url,cron_trigger_cancel_url,pt):
 
 
 def processing_job(cron_processing_url,pt):
-    max_retries = 5
+    max_retries = 3
     retry_count = 0
     pt(f"Processing ※ 執行中") 
 
     while retry_count < max_retries:
-        retry_count += 1
 
         try:
-            
-            response = requests.get(cron_processing_url)
+            response = requests.get(cron_processing_url, timeout=180)
             pt(f"Response data : {response.text}") 
-
+            responseData = response.json()
+            # 響應成功，重設 retry_count
+            retry_count = 0
+            if responseData.get('status')==403:  
+                pt(f"\nProcessing任務完成")
+                break
+            
+        except requests.Timeout:
+            retry_count += 1  # 發生超時錯誤後增加重試次數
+            pt(f"Timeout error ({retry_count}/{max_retries}): 請求超時，等待{timeout}秒")
+            if retry_count < max_retries:
+                time.sleep(10)  # 避免由於連接問題造成過度重試
+            else:
+                pt("警告: 停止運行，Processing超過錯誤上限")
+                break
+            
         except requests.RequestException as e:
+            retry_count += 1
             pt(f"Request error ({retry_count}/{max_retries}):" )
             pt(f"{e}")
             if retry_count < max_retries:
                 time.sleep(10)  # 避免由於連接問題造成過度重試
             else:
-                ValueError("警告: 停止運行，Processing超過嘗試上限")
+                ValueError("警告: 停止運行，Processing超過錯誤上限")
                 break
 
             continue
-
-        if "is not triggered. Request skipped." in response.text:
-            pt(f"\nProcessing任務完成")
-            break
 
 
 def run_process(param,pt=None, config_data=None):
